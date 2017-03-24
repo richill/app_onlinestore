160cb504b21ff6b5632e3b801656d2b11f6aebec
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-03-24 16:50:29 +0000

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, This is FirstChoice Website Page"
+    render html: "hello Mummy, This is Girl, This is where you will see all the live updates for your website - This is FirstChoice Website Page"
   end
 end
