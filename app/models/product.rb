07988e818ee47a030898bd854ea29d97a9d7d6b3
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-03-25 15:13:42 +0000

    updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,8 @@ class Product < ApplicationRecord
   belongs_to :category_accessorytype 
   belongs_to :category_bagtype 
   belongs_to :category_brand 
-  belongs_to :category_clothesizes
-  belongs_to :category_clothetypes 
+  belongs_to :category_clothesize
+  belongs_to :category_clothetype 
   belongs_to :category_colour 
   belongs_to :category_generalsize
   belongs_to :category_shoesize
