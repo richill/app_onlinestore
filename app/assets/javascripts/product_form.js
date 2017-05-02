2483ffdb1befc7383981f59f31b41d39f764dd47
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-05-02 22:55:50 +0100

    updated

--- app/assets/javascripts/product_form.js
+++ app/assets/javascripts/product_form.js
@@ -3,25 +3,47 @@
 //    application.js
 // ----------------------------
 
-
-
-
 $(function() {
     $('.row_dim').hide(); 
     $('#product_category_type_id').change(function(){
         if($('#product_category_type_id').val() == 1) {
             $('.clothes_type').show();
-        // else if($('#product_category_type_id').val() == 2) {
-        //     $('.accessories_type').show(); 
-        // else if($('#product_category_type_id').val() == 3) {
-        //     $('.shoes_type').show(); 
-        // else if($('#product_category_type_id').val() == 4) {
-        //     $('.bag_type').show(); 
-        } else {
-            $('.row_dim').hide(); 
+            $('.accessories_type').hide();
+            $('.shoes_type').hide();
+            $('.bag_type').hide();
+            $('.gift_type').hide();
+        } else if ($('#product_category_type_id').val() == 2) {
+            $('.accessories_type').show();
+            $('.clothes_type').hide();
+            $('.shoes_type').hide();
+            $('.bag_type').hide();
+            $('.gift_type').hide(); 
+        } else if ($('#product_category_type_id').val() == 3) {
+            $('.shoes_type').show();
+            $('.clothes_type').hide();
+            $('.accessories_type').hide();
+            $('.bag_type').hide();
+            $('.gift_type').hide(); 
+        } else if ($('#product_category_type_id').val() == 4) {
+            $('.bag_type').show();
+            $('.clothes_type').hide();
+            $('.accessories_type').hide();
+            $('.shoes_type').hide();
+            $('.gift_type').hide(); 
+        } else if ($('#product_category_type_id').val() == 5) {
+            $('.gift_type').show();
+            $('.clothes_type').hide();
+            $('.accessories_type').hide();
+            $('.shoes_type').hide();
+            $('.bag_type').hide(); 
         } 
     });
 });
 
-
+$(function() {
+  $('.product_saleprice').hide(); 
+  $('#product_sale').click(function(){
+    $('.product_saleprice').toggle();
+  });
+});
 
