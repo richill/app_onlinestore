9d6b7dd07ea2a8b91444f3271075dba26c15abc8
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-05-13 11:51:57 +0100

    updated

---
+++ app/assets/javascripts/products_form.js
@@ -0,0 +1,51 @@
+
+// ----------------------------
+//    application.js
+// ----------------------------
+
+$(function() {
+    $('.row_dim').hide(); 
+    $('#product_category_type_id').change(function(){
+        if($('#product_category_type_id').val() == 1) {
+            $('.clothes_type').show();
+            $('.accessories_type').hide();
+            $('.shoes_type').hide();
+            $('.bag_type').hide();
+            $('.gift_type').hide();
+        } else if ($('#product_category_type_id').val() == 2) {
+            $('.accessories_type').show();
+            $('.clothes_type').hide();
+            $('.shoes_type').hide();
+            $('.bag_type').hide();
+            $('.gift_type').hide(); 
+        } else if ($('#product_category_type_id').val() == 3) {
+            $('.shoes_type').show();
+            $('.clothes_type').hide();
+            $('.accessories_type').hide();
+            $('.bag_type').hide();
+            $('.gift_type').hide(); 
+        } else if ($('#product_category_type_id').val() == 4) {
+            $('.bag_type').show();
+            $('.clothes_type').hide();
+            $('.accessories_type').hide();
+            $('.shoes_type').hide();
+            $('.gift_type').hide(); 
+        } else if ($('#product_category_type_id').val() == 5) {
+            $('.gift_type').show();
+            $('.clothes_type').hide();
+            $('.accessories_type').hide();
+            $('.shoes_type').hide();
+            $('.bag_type').hide(); 
+        } else {
+            $('.row_dim').hide();
+        }  
+    });
+});
+
+$(function() {
+  $('.product_saleprice').hide(); 
+  $('#product_sale').click(function(){
+    $('.product_saleprice').toggle();
+  });
+});
+
---
+++ app/assets/javascripts/products_show.js
@@ -0,0 +1,9 @@
+// ----------------------------------------
+//    Section: Product Show
+//    File: products / show.html.erb
+// ----------------------------------------
+
+$(document).ready(function() {
+  // Initialize the plugin
+  $('#my_popup').popup();
+});
