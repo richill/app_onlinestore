316b66ab94db80657dc4b6096df21b4641e70ad7
Richill Tamakloe <richill.tamakloe@gmail.com>
2017-05-17 11:46:06 +0100

    jaavscript url id working

--- app/assets/javascripts/products_index.js
+++ app/assets/javascripts/products_index.js
@@ -3,54 +3,141 @@
 //    File: products / index.html.erb
 // ----------------------------------------
 
+// $(document).ready(function () {
+//   function openOverlay(olEl) {
+//     $oLay = $(olEl);
+
+//     if ($('#overlay-shade').length == 0)
+//       $('body').prepend('<div id="overlay-shade"></div>');
+
+//     $('#overlay-shade').fadeTo(300, 0.6, function() {
+//       var props = {
+//         oLayWidth: $oLay.width(),
+//         scrTop: $(window).scrollTop(),
+//         viewPortWidth: $(window).width()
+//       };
+
+//       var leftPos = (props.viewPortWidth - props.oLayWidth) / 2;
+
+//       $oLay
+//         .css({
+//           display: 'block',
+//           opacity: 0,
+//           top: '-=300',
+//           left: leftPos + 'px'
+//         })
+//         .animate({
+//           top: props.scrTop + 40,
+//           opacity: 1
+//         }, 600);
+//     });
+//   }
+
+//   function closeOverlay() {
+//     $('.overlay').animate({
+//       top: '-=300',
+//       opacity: 0
+//     }, 400, function() {
+//       $('#overlay-shade').fadeOut(300);
+//       $(this).css('display', 'none');
+//     });
+//   }
+
+//   $('#overlay-shade, .overlay').on('click', 'a', function(e) {
+//     closeOverlay();
+//     if ($(this).attr('href') == '#') e.preventDefault();
+//   });
+
+//   // Usage
+//   $('#overlaylaunch-inAbox').click(function(e) {
+//     openOverlay('#overlay-inAbox');
+//     e.preventDefault();
+//   });
+// });
+
+
+
+
+
 $(document).ready(function () {
-  function openOverlay(olEl) {
-    $oLay = $(olEl);
 
-    if ($('#overlay-shade').length == 0)
-      $('body').prepend('<div id="overlay-shade"></div>');
+  $(function() {
 
-    $('#overlay-shade').fadeTo(300, 0.6, function() {
-      var props = {
-        oLayWidth: $oLay.width(),
-        scrTop: $(window).scrollTop(),
-        viewPortWidth: $(window).width()
-      };
+    var o = getParameterByName('url_id');
 
-      var leftPos = (props.viewPortWidth - props.oLayWidth) / 2;
-
-      $oLay
-        .css({
-          display: 'block',
-          opacity: 0,
-          top: '-=300',
-          left: leftPos + 'px'
-        })
-        .animate({
-          top: props.scrTop + 40,
-          opacity: 1
-        }, 600);
-    });
-  }
-
-  function closeOverlay() {
-    $('.overlay').animate({
-      top: '-=300',
-      opacity: 0
-    }, 400, function() {
-      $('#overlay-shade').fadeOut(300);
-      $(this).css('display', 'none');
-    });
-  }
-
-  $('#overlay-shade, .overlay').on('click', 'a', function(e) {
-    closeOverlay();
-    if ($(this).attr('href') == '#') e.preventDefault();
+    if('hello' == o){
+      $('.my_awesom_botton').trigger( "click" );
+    }
   });
 
-  // Usage
-  $('#overlaylaunch-inAbox').click(function(e) {
-    openOverlay('#overlay-inAbox');
-    e.preventDefault();
-  });
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 });
+
+
+
+
+
+// $(function() {
+
+//    var o = getParameterByName('opennnnn');
+
+//    if('ggggg' == o){
+//         $('.my_awesom_botton').trigger( "click" );
+//     }
+// });
+
+// function getParameterByName(name, url) {
+//     if (!url) url = window.location.href;
+//     name = name.replace(/[\[\]]/g, "\\$&");
+//     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+//         results = regex.exec(url);
+//     if (!results) return null;
+//     if (!results[2]) return '';
+//     return decodeURIComponent(results[2].replace(/\+/g, " "));
+// }
+
+
+// <a class="my_awesom_botton" href="http://admin.lps.dev/admin/?opennnnn=ggggg" class="btn">one</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
